74212b46b7b287b73ee49312b179ab7f40de94f5
Owen Anderson <resistor@mac.com>
2008-01-26 16:37:24 -0600

    Reorganize some stuff.  Since RSL is single-file, we don't need a separate source manager from the lexer.

---
+++ include/rsl-llvm/Lexer.h
@@ -0,0 +1,23 @@
+#include <llvm/Support/MemoryBuffer.h>
+#include <string>
+
+#ifndef RSL_LEX_LEXER_H
+#define RSL_LEX_LEXER_H
+
+class Lexer {
+private:
+  const llvm::MemoryBuffer* Buffer;
+  
+  size_t* SourceLineCache;
+  unsigned NumLines;
+public:
+  
+  Lexer(const llvm::MemoryBuffer* buf) : Buffer(buf) {
+    SourceLineCache = 0;
+    NumLines = 0;
+  }
+  
+  void computeLineNumbers();
+};
+
+#endif
---
+++ lib/LexRSL/Lexer.cpp
@@ -0,0 +1,42 @@
+#include "rsl-llvm/Lexer.h"
+#include <vector>
+#include <algorithm>
+
+void Lexer::computeLineNumbers() {
+  // Find the file offsets of all of the *physical* source lines.  This does
+  // not look at trigraphs, escaped newlines, or anything else tricky.
+  std::vector<unsigned> LineOffsets;
+  
+  // Line #1 starts at char 0.
+  LineOffsets.push_back(0);
+  
+  const unsigned char *Buf = (const unsigned char *)Buffer->getBufferStart();
+  const unsigned char *End = (const unsigned char *)Buffer->getBufferEnd();
+  unsigned Offs = 0;
+  while (1) {
+    // Skip over the contents of the line.
+    const unsigned char *NextBuf = (const unsigned char *)Buf;
+    while (*NextBuf != '\n' && *NextBuf != '\r' && *NextBuf != '\0')
+      ++NextBuf;
+    Offs += NextBuf-Buf;
+    Buf = NextBuf;
+    
+    if (Buf[0] == '\n' || Buf[0] == '\r') {
+      // If this is \n\r or \r\n, skip both characters.
+      if ((Buf[1] == '\n' || Buf[1] == '\r') && Buf[0] != Buf[1])
+        ++Offs, ++Buf;
+      ++Offs, ++Buf;
+      LineOffsets.push_back(Offs);
+    } else {
+      // Otherwise, this is a null.  If end of file, exit.
+      if (Buf == End) break;
+      // Otherwise, skip the null.
+      ++Offs, ++Buf;
+    }
+  }
+  
+  // Copy the offsets into the FileInfo structure.
+  NumLines = LineOffsets.size();
+  SourceLineCache = new size_t[LineOffsets.size()];
+  std::copy(LineOffsets.begin(), LineOffsets.end(), SourceLineCache);
+}
